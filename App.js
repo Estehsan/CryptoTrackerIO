b6b28871006f03091275fd172eb96c803d48be0b
estehsan <games.huseb@email.com>
2022-01-31 17:23:31 +0500

    Home Screen Added

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, SafeAreaView } from "react-native";
+import cryptocurrencies from "./src/data/cryptocurrencies.json";
+import CoinItemComp from "./src/component/CoinItem/CoinItemComp";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={cryptocurrencies}
+        renderItem={({ item }) => <CoinItemComp marketcoin={item} />}
+      />
+
+      {/* <Text>Yes up App.js to start working on your app!</Text> */}
+
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    padding: 40,
   },
 });
